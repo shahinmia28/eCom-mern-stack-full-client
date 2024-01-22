d6ce78162135433e1031fad8fa56ee40ada25141
shahinmia28 <shahin01777296933@gmail.com>
2024-01-22 20:50:55 +0600

    gg

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,5 +1,2 @@
-const API = process.env.REACT_APP_API_URL_LOCAL;
+const API = process.env.REACT_APP_API_URL;
 export default API;
-
-// console.log(process.env.REACT_APP_API_URL);
-// console.log(process.env.REACT_APP_API_URL_LOCAL);
